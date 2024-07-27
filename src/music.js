4c14ed8c25456bb1dbe1cbd2c70f38e42fba5756
JANANI-B-S <143819251+JANANI-B-S@users.noreply.github.com>
2024-07-27 21:58:24 +0530

    Add files via upload

---
+++ src/music.js
@@ -0,0 +1,43 @@
+let allMusic = [
+    {
+        name: "Kandha Sasti Kavasam",
+        artist:"Murugan",
+        img: "kandha",
+        src: "kandha"
+    },
+
+    {
+        name: "Kandha Guru Kavasam",
+        artist:"Murugan",
+        img: "guru",
+        src: "guru"
+    },
+
+    {
+        name: "SivaPuranam",
+        artist:"Shivan",
+        img: "sivapuranam",
+        src: "sivapuranam"
+    },
+
+    {
+        name: "Lalitha Sahasaranam",
+        artist:"Amman",
+        img: "lalitha",
+        src: "lalitha"
+    },
+
+    {
+        name: "Vishnu Sahasaranam",
+         artist:"Vishnu",
+        img: "vishnu",
+        src: "vishnu"
+    },
+
+    {
+        name: "Kolaru Pathipagam",
+        artist: "Shivan",
+        img: "kolaru",
+        src: "kolaru"
+    }
+]
